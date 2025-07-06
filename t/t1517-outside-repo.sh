25156ed8735f7d63af69d1697381eeec4104735c
Usman Akinyemi <usmanakinyemi202@gmail.com>
2025-07-07 03:20:34 +0530

    t/t1517: move for-each-ref -h test to t1517
    The test 'for-each-ref does not crash with -h' checks that
    the command exits cleanly with usage information, both inside and
    outside of a repository.
    
    Move this test from "t6300-for-each-ref.sh" to
    "t1517-outside-repo.sh" since it better fits with tests that check
    command behavior outside a repository.
    
    Suggested-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Usman Akinyemi <usmanakinyemi202@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1517-outside-repo.sh
+++ t/t1517-outside-repo.sh
@@ -121,4 +121,11 @@ test_expect_success 'checkout-index does not crash with -h' '
 	test_grep "[Uu]sage: git checkout-index " usage
 '
 
+test_expect_success 'for-each-ref does not crash with -h' '
+	test_expect_code 129 git for-each-ref -h >usage &&
+	test_grep "[Uu]sage: git for-each-ref " usage &&
+	test_expect_code 129 nongit git for-each-ref -h >usage &&
+	test_grep "[Uu]sage: git for-each-ref " usage
+'
+
 test_done
